542f8ed62c5129bc3e92c0e47adc98b6e9ce88f4
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-09 20:54:12 +0000

    Add font awesome icons and organise menu items layout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,10 +8,19 @@ const NavBar = () => {
    <Container>
   <Navbar.Brand href="#home"><img src={logo} height="60px" alt="site logo"/></Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
+  <Navbar.Collapse bg="light" id="basic-navbar-nav">
+    <Nav className="ml-auto text-right">
+      <Nav.Link>
+        Home
+        <i className="fa-solid fa-house"></i></Nav.Link>
+      <Nav.Link>
+      Sign In
+      <i className="fa-solid fa-right-to-bracket"></i>
+      </Nav.Link>
+      <Nav.Link>
+        Sign Up
+        <i className="fa-solid fa-user-plus"></i>
+        </Nav.Link>
     </Nav>
   </Navbar.Collapse>
   </Container>
