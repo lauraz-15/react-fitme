0051be57e98e1875353833c0d4017c79107acf65
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-10 11:25:31 +0000

    Add Routing settings and link nav links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import styles from './App.module.css';
-import { Button } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import NavBar from './components/NavBar';
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar/>
+      <Container className={styles.Body}>
+      <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,30 @@ import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import logo from '../assets/logo-resized.png'
 import styles from '../styles/NavBar.module.css'
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
 <Navbar className={styles.NavBar} bg="light" expand="md" fixed="top">
    <Container>
-  <Navbar.Brand href="#home"><img src={logo} height="60px" alt="site logo"/></Navbar.Brand>
+    <NavLink to="/">
+      <Navbar.Brand href="#home"><img src={logo} height="60px" alt="site logo"/></Navbar.Brand>
+    </NavLink>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse bg="light" id="basic-navbar-nav">
     <Nav className="ml-auto text-right">
-      <Nav.Link>
+      <NavLink to="/">
         Home
-        <i className="fa-solid fa-house"></i></Nav.Link>
-      <Nav.Link>
+        <i className="fa-solid fa-house"></i>
+      </NavLink>
+      <NavLink to="/signin">
       Sign In
       <i className="fa-solid fa-right-to-bracket"></i>
-      </Nav.Link>
-      <Nav.Link>
+      </NavLink>
+      <NavLink to="/signup">
         Sign Up
         <i className="fa-solid fa-user-plus"></i>
-        </Nav.Link>
+      </NavLink>
     </Nav>
   </Navbar.Collapse>
   </Container>
