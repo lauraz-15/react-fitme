bef343f24cf87a6629148c270474fb0d6ec764d5
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-10 19:27:19 +0000

    Move SignUpForm.js file from components to pages-ath directory

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import styles from './App.module.css';
 import { Button, Container } from 'react-bootstrap';
 import NavBar from './components/NavBar';
-import SignUpForm from './components/SignUpForm';
+
 import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/login" render={() => <h1>Log in</h1>} />
-          <Route exact path="/signup" render={() => <SignUpForm/>} />
+          <Route exact path="/signup" render={() => <SignUpForm/>  } />
           <Route render={() => <h1>Page not found! :(</h1>} />
         </Switch>
       </Container>
--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,40 +1,103 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 
 import styles from "../../styles/SignUpLogInForm.module.css";
 import btnStyles from "../../styles/Buttons.module.css";
 import appStyles from "../../App.module.css";
-
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
+import axios from "axios";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const SignUpForm = () => {
+    const [signUpInfo, setSignupInfo] = useState({
+        username: "",
+        password1: "",
+        password2: "",
+      });
+    
+    const { username, password1, password2 } = signUpInfo;
+    const history = useHistory();
+    const [errors, setErros] = useState({
+
+    })
+    const handleChange = (event) => {
+        setSignupInfo({
+            ...signUpInfo,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSumbit = async (event) => {
+        event.preventDefault();
+        try {
+          await axios.post('/dj-rest-auth/registration/', signUpInfo)
+          history.push('/login')
+        } catch(err){
+            setErros(err.response?.data)
+        }
+    }
+
   return (
-    <Row className={styles.Row}>
-      <Col className="my-auto py-2 p-md-2" md={6}>
-        <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign up</h1>
-
-            {/* add your form here */}
-
+    <div className={appStyles.Body}>
+        <Container fluid="md">
+           <Row className="justify-content-md-center">
+            <Col lg={4}>
+                <h1>Sign Up</h1>
+                <Form onSubmit={handleSumbit}>
+                <Form.Group controlId="username">
+                    <Form.Label className="d-none">Username</Form.Label>
+                    <Form.Control 
+                    type="text" 
+                    placeholder="username" 
+                    name="username" 
+                    value={username}
+                    onChange={handleChange}/>
+                </Form.Group>
+                    {errors.username?.map((message, idx) => (
+                        <Alert variant="warning" key={idx}>{message}</Alert>
+                    ))}
+                <Form.Group controlId="password1">
+                    <Form.Label className="d-none">Password</Form.Label>
+                    <Form.Control 
+                    type="password" 
+                    placeholder="Password" 
+                    name="password1" 
+                    value={password1}
+                    onChange={handleChange}/>
+                </Form.Group>
+                    {errors.password1?.map((message, idx) => (
+                        <Alert variant="warning" key={idx}>{message}</Alert>
+                    ))}
+                <Form.Group controlId="password2">
+                    <Form.Label className="d-none">Password</Form.Label>
+                    <Form.Control 
+                    type="password" 
+                    placeholder="Confirm Password" 
+                    name="password2" 
+                    value={password2}
+                    onChange={handleChange}/>
+                </Form.Group>
+                {errors.password2?.map((message, idx) => (
+                        <Alert variant="warning" key={idx}>{message}</Alert>
+                    ))}          
+                <Button className={btnStyles.Button} variant="primary" type="submit">
+                    Sign Up
+                </Button>
+                {errors.non_field_errors?.map((message, idx) => (
+                        <Alert variant="warning" className="mt-4" key={idx}>{message}</Alert>
+                    ))}
+            </Form>
+        <div className={styles.LogIn}>
+             <h4>Registered users, please log in here:</h4>
+             <Link to="/login">
+                <Button className={btnStyles.Button}>Log In</Button>
+            </Link>
+        </div>
+            </Col>
+        </Row>
         </Container>
-        <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/login">
-            Already have an account? <span>Log in</span>
-          </Link>
-        </Container>
-      </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
-      >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          src={
-            "https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"
-          }
-        />
-      </Col>
-    </Row>
+    </div>
+ 
   );
 };
 
