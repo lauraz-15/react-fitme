6d7d5ee698522091e50c10e61ba05e8344fc082a
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-11 11:29:31 +0000

    Reorganise menu items

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -20,18 +20,11 @@ const NavBar = () => {
     }
  }
 
-  const addImage = (
-    <NavLink to="/images/add"  className={styles.NavLink} activeClassName={styles.Active}>
-   <i className="fa-solid fa-square-plus"></i>
-    </NavLink>
-  )
   const notAuthenticatedView = (
     <>
     <NavLink to="/login"  className={styles.NavLink} activeClassName={styles.Active}>
     Log In
     <i className="fa-solid fa-right-to-bracket"></i>
-    <i className="fa-solid fa-square-plus"></i>
-    <i className="far fa-plus-squere"></i>
     </NavLink>
     <NavLink to="/signup"  className={styles.NavLink} activeClassName={styles.Active}>
       Sign Up
@@ -43,24 +36,25 @@ const NavBar = () => {
 
   const authenticatedView = (
         <>
+        <NavLink to="/main" exact className={styles.NavLink} activeClassName={styles.Active}>
+        <i className="fa-solid fa-house"></i>
+        </NavLink>
+        <NavLink to="/images/add"  className={styles.NavLink} activeClassName={styles.Active}>
+        <i className="fa-solid fa-square-plus"></i>
+        </NavLink>
         <NavLink to="/kudos"  className={styles.NavLink} activeClassName={styles.Active}>
         <i className="fa-solid fa-thumbs-up"></i>
-        kudos
         </NavLink>
-
         <NavLink to="/" 
         className={styles.NavLink}
         onClick={handleLogOut}>
         <i className="fa-solid fa-person-walking-arrow-right"></i>
-        sign out
         </NavLink>
         <NavLink to={`/accounts/${currUser?.account_id}`}
         className={styles.NavLink}>
-          profile
         <Profile src={currUser?.account_image} text="Account"/>
         </NavLink>
-        </>
-        
+        </>   
   )
 
   return (
@@ -69,14 +63,9 @@ const NavBar = () => {
     <NavLink to="/">
       <Navbar.Brand><img src={logo} height="60px" alt="site logo"/></Navbar.Brand>
     </NavLink>
-    {currUser && addImage}
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse bg="light" id="basic-navbar-nav">
     <Nav className="ml-auto text-right">
-      <NavLink to="/" exact className={styles.NavLink} activeClassName={styles.Active}>
-        Home
-        <i className="fa-solid fa-house"></i>
-      </NavLink>
     {currUser ? authenticatedView : notAuthenticatedView}
     </Nav>
   </Navbar.Collapse>
