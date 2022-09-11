bc63446e0946d6d0c07987445b7bc325dc30f0c4
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-11 20:42:17 +0000

    Add useEffct to get the image detail data

--- src/pages/images/ImageDetail.js
+++ src/pages/images/ImageDetail.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { useParams } from 'react-router';
+import axios from 'axios';
+import { axiosReq } from '../../api/axiosDefaults';
+
 
 const ImageDetail = () => {
+    const { id } = useParams();
+    const [image, setImage] = useState({ results: [] })
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const [{ data: image }] = await Promise.all([
+                    axiosReq.get(`/images/${id}`),
+                ])
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        handleMount();
+    }, [id])
     return (
         <Row className="h-100">
           <Col className="py-2 p-0 p-lg-2" lg={8}>
