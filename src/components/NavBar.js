2f797ec944b94041f13b9f50388fada17fc94239
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-11 10:37:47 +0000

    Add extra menu items on the nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,11 +8,18 @@ import { useCurrUser } from '../contexts/CurrUserContext';
 const NavBar = () => {
   const currUser = useCurrUser();
 
+  const addImage = (
+    <NavLink to="/images/add"  className={styles.NavLink} activeClassName={styles.Active}>
+   <i className="fa-solid fa-square-plus"></i>
+    </NavLink>
+  )
   const notAuthenticatedView = (
     <>
     <NavLink to="/login"  className={styles.NavLink} activeClassName={styles.Active}>
     Log In
     <i className="fa-solid fa-right-to-bracket"></i>
+    <i className="fa-solid fa-square-plus"></i>
+    <i className="far fa-plus-squere"></i>
     </NavLink>
     <NavLink to="/signup"  className={styles.NavLink} activeClassName={styles.Active}>
       Sign Up
@@ -20,14 +27,34 @@ const NavBar = () => {
     </NavLink>
     </>
   )
-  const authenticatedView = <>{currUser?.username}</>
+
+
+  const authenticatedView = (
+        <>
+        <NavLink to="/kudos"  className={styles.NavLink} activeClassName={styles.Active}>
+        <i className="fa-solid fa-thumbs-up"></i>
+        </NavLink>
+        <NavLink to="/" 
+        className={styles.NavLink}
+        onClick={() => {}}>
+        <i className="fa-solid fa-person-walking-arrow-right"></i>
+        </NavLink>
+        <NavLink to={`/accounts/${currUser?.account_id}`}
+        className={styles.NavLink}
+        onClick={() => {}}>
+        <img src={currUser?.account_image} height="50px;"/>
+        </NavLink>
+        </>
+        
+  )
 
   return (
-<Navbar className={styles.NavBar} bg="light" expand="md" fixed="top">
+<Navbar className={styles.NavBar}  expand="md" fixed="top">
    <Container>
     <NavLink to="/">
       <Navbar.Brand><img src={logo} height="60px" alt="site logo"/></Navbar.Brand>
     </NavLink>
+    {currUser && addImage}
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse bg="light" id="basic-navbar-nav">
     <Nav className="ml-auto text-right">
