22ade516e2d1fc4fe709317d98f137d85c222749
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-12 10:35:11 +0000

    Add kudos and commnets and updated_on items to the card body

--- src/pages/images/Image.js
+++ src/pages/images/Image.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Card, Media } from 'react-bootstrap';
+import { Card, Media, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Profile from '../../components/Profile';
 import { useCurrUser } from '../../contexts/CurrUserContext'
 
 const Image = (props) => {
     const {
-        id, owner, account_id, account_image, comments_count, kudos_count,
+        id, owner, account_id, account_image, comments_count, kudos_id, kudos_count,
         updated_on, description, picture, imagePage
     } = props
 
@@ -25,12 +25,34 @@ const Image = (props) => {
                 </div>
             </Media>
         </Card.Body>
+        
         <Link to={`/images/${id}`}>
         <Card.Img src={picture} alt={description} />
-        <span>{updated_on}</span>
         </Link>
-        <Card.Body>
-    
+        <Card.Body className="align-items-center justify-content-between">
+        
+        <Media className="align-items-center justify-content-between">
+                <div>
+                {is_owner ? (<OverlayTrigger
+                    placement="top"
+                    overlay={<Tooltip>You can't like your own post!</Tooltip>}>
+                    <i className="fa-regular fa-thumbs-up" />
+                    </OverlayTrigger>
+                ) : kudos_id ? (<span><i className="fa-solid fa-thumbs-up" /></span>
+                ) : currUser ? (<span><i className="fa-regular fa-thumbs-up" /></span>
+                ) : (<OverlayTrigger
+                    placement="top"
+                    overlay={<Tooltip>Log in to like posts!</Tooltip>}>
+                    <i className="fa-regular fa-thumbs-up" />
+                    </OverlayTrigger>)}
+                {kudos_count}
+                <Link to={`/images/${id}`}><i className="fa-regular fa-comment" /></Link>
+                {comments_count}
+                </div>    
+                <div className="d-flex align-items-center">
+                {updated_on}
+                </div>
+            </Media>
         </Card.Body>
 </Card>
   )
