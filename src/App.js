fb77852745afd94752e9d463e2c280e258bd1aab
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-12 14:40:53 +0000

    Add home page, main page and kudos routes and add filtering settings

--- src/App.js
+++ src/App.js
@@ -10,9 +10,12 @@ import AddImageForm from './pages/images/AddImageForm';
 import ImageDetail from './pages/images/ImageDetailPage';
 import ImageDetailPage from './pages/images/ImageDetailPage';
 import ImagesPage from './pages/images/ImgesPage';
+import { useCurrUser } from './contexts/CurrUserContext';
 
 
 function App() {
+  const currUser = useCurrUser();
+  const account_id = currUser?.account_id || "";
 
   return (
     <div className={styles.App}>
@@ -20,6 +23,12 @@ function App() {
       <Container className={styles.Body}>
       <Switch>
           <Route exact path="/" render={() => <ImagesPage/> } />
+          <Route exact path="/main" render={() => <ImagesPage 
+          message="No Images to show yet, please follow some users to see conent!"
+          filter={`owner__followed__owner__account=${account_id}&`}/> } />
+          <Route exact path="/kudos" render={() => <ImagesPage 
+          message="You haven't given kudos to any images yet :("
+          filter={`kudos__owner__account=${account_id}&ordering=-kudos__created_on&`}/> } />
           <Route exact path="/login" render={() => <LogInForm/> } />
           <Route exact path="/signup" render={() => <SignUpForm/>} />
           <Route exact path="/images/add" render={() => <AddImageForm/> } />
