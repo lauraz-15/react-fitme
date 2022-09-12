fea3b935870930be894a293c687e396a82ddf55d
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-12 15:26:29 +0000

    Add useEffect and return statement to ImagesPage.js

--- src/pages/images/ImgesPage.js
+++ src/pages/images/ImgesPage.js
@@ -1,19 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+// import Image from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 
-import appStyles from "../../App.module.css";
+// import nothing_found from "../../assets/nothing_found"
+import Image from "./Image";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
-function ImagesPage() {
-  
+function ImagesPage({ message, filter = "" }) {
+  const [images, setImages] = useState({ results: [] });
+  const [loaded, setLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchImages = async () => {
+        try {
+            const {data} = await axiosReq.get(`/images/${filter}`)
+            setImages(data)
+            setLoaded(true)
+        } catch(err) {
+            console.log(err)
+        }
+    } 
+    setLoaded(false)
+    fetchImages()
+  }, [filter, pathname]);
+ 
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         something
-        <p>Images</p>
+        {loaded ? (
+          <>
+            {images.results.length ? (
+              images.results.map((image) => (
+                <Image key={image.id} {...image} setImages={setImages} />
+              ))
+            ) : (
+            //   <Container>
+            //     <Image src={nothing_found}/>
+            //   </Container>
+            <p>nothing found</p>
+            )}
+          </>
+        ) : (
+          <Container>
+            spinning
+          </Container>
+        )}
       </Col>
     </Row>
   );
