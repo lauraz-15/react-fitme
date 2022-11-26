6579311209144d67e52ac0fcfb40d3667b10c262
lauraz-15 <laura.zvirbule90@gmail.com>
2022-11-26 20:14:35 +0000

    Update the backend api link on axiosDefaults.js file

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-axios.defaults.baseURL = "https://drf-fitme.herokuapp.com/"
+axios.defaults.baseURL = "https://drf-api-sa31.onrender.com"
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
